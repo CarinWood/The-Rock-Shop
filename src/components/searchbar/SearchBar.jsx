d22b6da27f893c2085fc54664bb6b4990dbb7483
CarinWood <carin.wood.85@gmail.com>
2022-01-26 18:48:29 +0100

    update searchbar

--- src/components/searchbar/SearchBar.jsx
+++ src/components/searchbar/SearchBar.jsx
@@ -1,14 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './searchbar.css';
+import Albums from '../../data/AlbumData';
+
 
 const SearchBar = () => {
+
+
+  const [searchValue, setSearchValue] = useState('')
+
+  function handleInputchange(e) {
+    setSearchValue(e.target.value);
+  }
+
+  function handleClearClick() {
+    setSearchValue('');
+  }
+
+  const shouldDisplayDeleteBtn = searchValue.length > 0;
+  const displayResults = searchValue.length > 0;
+
+ 
+
   return (
+    <>
   <form>
       <input 
       className='searchbar' 
       type="text" 
-      placeholder="Search..." />
+      placeholder="Sök..."
+      value={searchValue}
+      onChange={handleInputchange}
+      />
+
+      {shouldDisplayDeleteBtn && 
+         <button 
+            className='delete-input'
+            type="button" 
+            onClick={handleClearClick}>
+                <i class='bx bx-x'></i>
+         </button>
+         }
   </form>
+
+         {displayResults &&
+              <ul className='result-list'>
+                  {Albums.map((album) => (
+                        <li key={album.id} className='result-item'>{album.artist}</li>
+                  ))}
+              </ul>
+       }
+       </>
+  
   )
 };
 
