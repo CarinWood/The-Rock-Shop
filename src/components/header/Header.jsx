d5819264f6fafd1d8c07678f5cf5ee5a97c5ed56
CarinWood <carin.wood.85@gmail.com>
2022-01-27 20:40:02 +0100

    fix with log in and log out

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,29 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import SearchBar from '../searchbar/SearchBar';
 import './header.css'
 import { FaShoppingBasket } from 'react-icons/fa';
 import NavBar from '../navbar/NavBar';
 import Cart from '../cart/Cart';
+import { LoginContext } from '../../data/LoginProvider';
+import { useNavigate, useNavigation } from 'react-router-dom'
 
 
 const Header = () => {
+
+  const navigation = useNavigate()
+
+  const {loggedIn, setLoggedIn} = useContext(LoginContext);
 
   const [ShowingCart, setShowingCart] = useState(false)
 
   function showCart() {
     setShowingCart(!ShowingCart)
  
+  }
+
+  function logOutFunc() {
+    setLoggedIn(!loggedIn)
+    navigation("/")
   }
 
   return (
@@ -22,6 +33,9 @@ const Header = () => {
       <div className='logo'> The Rock Shop</div>
      
       <SearchBar/>
+
+          {loggedIn? <p className='membername'>Carin <span onClick={logOutFunc} className='logout'>(Logga ut)</span></p>: null}
+
           <div className='basket-div'>
               <FaShoppingBasket className="basket-icon" />
               <p className='digit'>0</p>
--- src/pages/signedin/SignedIn.jsx
+++ src/pages/signedin/SignedIn.jsx
@@ -20,7 +20,7 @@ const SignedIn = () => {
         <div className='main-text'>
           <h1><span className='logotype'>The Rock Shop</span> <span className='club'>Club</span></h1>
           <p className='membership-level'>Medlemsnivå: <span className='gold'> GULD</span></p>
-          <p className='text'>Just nu bjuder vi alla våra guldmedlemmar på 10% rabatt på ditt nästa köp av konsertbiljett!
+          <p className='text'>Just nu bjuder vi alla våra guldmedlemmar på 10% rabattvid köp av valfri konsertbiljett!
              Använd koden: <span className='sale'>RockShop10</span> när du handlar på <span className='tickets'>konsertbiljetter.se</span>
           </p>
           <p>Erbjudandet gäller t.o.m. den 21/3 - 2022</p>
