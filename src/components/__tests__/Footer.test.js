ada9b55b67f0a89cb4e4fa7b5c71845ac50a1a9f
CarinWood <carin.wood.85@gmail.com>
2022-02-02 19:06:09 +0100

    update test

--- src/components/__tests__/Footer.test.js
+++ src/components/__tests__/Footer.test.js
@@ -2,7 +2,6 @@ import { render } from '@testing-library/react';
 import Footer from '../footer/Footer';
 
 test('check if footer renders with correct text', () => {
-    const component = render(<Footer/>)
-    const footertext = component.getByTestId('footertext')
-    expect(footertext.textContent).toBe('Created and built by Carin Wood')
+    const {getByTestId} = render(<Footer/>)
+    expect(getByTestId('footertext').textContent).toBe('Created and built by Carin Wood')
 })
