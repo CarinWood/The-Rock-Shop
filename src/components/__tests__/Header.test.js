fba1b1682e8ec0cf15d65f68b0e08db01e77f554
CarinWood <carin.wood.85@gmail.com>
2022-02-03 19:43:15 +0100

    update test

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -1,11 +1,15 @@
-import { render } from '@testing-library/react';
+import { getByTestId, render } from '@testing-library/react';
 import App from '../../App';
 
 
 test('test if renders logo text correct', () => {
-    const component = render(<App/>)
-    const headerElement = component.getByTestId('logotext') 
-    expect(headerElement.textContent).toBe('The Rock Shop')
+    const {getByTestId} = render(<App/>)
+    expect(getByTestId('logotext').textContent).toBe('The Rock Shop')
 })
 
+
+test("check if button renders with correct text", () => {
+    const {getByTestId} = render(<App/>)
+    expect(getByTestId('cart-btn').textContent).toBe("Varukorg")
+})
 
